5352841d1781170043ef1e90b48f718ba2a18195
unknown <arkanfaith4@gmail.com>
2026-03-14 21:40:27 +0700

    fix vercel laravel entrypoint

--- api/index.php
+++ api/index.php
@@ -1,3 +1,15 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+$_ENV['APP_RUNNING_IN_CONSOLE'] = false;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
